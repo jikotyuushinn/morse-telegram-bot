abe426bcc7f4f87afe5516198ed6d064a22f5a94
Kasper4649 <imzxz123456@gmail.com>
2021-01-17 18:54:50 +0800

    [fix] fix bot always reply

--- main.go
+++ main.go
@@ -44,40 +44,34 @@ func main() {
 		log.Printf("From: %+v Chat: %+v Text: %+v\n", update.Message.From, update.Message.Chat.ID,
 			update.Message.Text)
 		
-		var response tgbotapi.MessageConfig
-		
 		if update.Message.IsCommand() {
 			switch command := update.Message.Command(); command {
 			case "start":
-				response = tgbotapi.NewMessage(update.Message.Chat.ID, "不準開始")
+				_, _ = bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "不準開始"))
 			case "help":
-				response = tgbotapi.NewMessage(update.Message.Chat.ID, "禁止幫助")
+				_, _ = bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "禁止幫助"))
 			case "decode":
 				morseCode := strings.TrimLeft(update.Message.Text, "/decode ")
 				if morseCode == "" {
-					response = tgbotapi.NewMessage(update.Message.Chat.ID, "勸你最好有輸入")
+					_, _ = bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "勸你最好有輸入"))
 					break
 				}
 				res, _ := controller.JsParser(util.StaticPath, "xmorse.decode", morseCode)
-				response = tgbotapi.NewMessage(update.Message.Chat.ID, res)
+				_, _ = bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, res))
 				_, _ = bot.Send(tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID))
 			case "encode":
 				text := strings.TrimLeft(update.Message.Text, "/encode ")
 				if text == "" {
-					response = tgbotapi.NewMessage(update.Message.Chat.ID, "勸你最好有輸入")
+					_, _ = bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "勸你最好有輸入"))
 					break
 				}
 				res, _ := controller.JsParser(util.StaticPath, "xmorse.encode", text)
-				response = tgbotapi.NewMessage(update.Message.Chat.ID, res)
+				_, _ = bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, res))
 				_, _ = bot.Send(tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID))
 			default:
-				response = tgbotapi.NewMessage(update.Message.Chat.ID, "不要亂玩人家哦。")
+				_, _ = bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "不要亂玩人家哦。"))
 			}
-		} else {
-			response = tgbotapi.NewMessage(update.Message.Chat.ID, "這位先生，本小姐不陪聊。")
 		}
-		
-		_, _ = bot.Send(response)
 	})
 	
 	err = router.Run()

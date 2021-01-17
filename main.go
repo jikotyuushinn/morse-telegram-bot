709a061d92840ef430b48efbc400e18958fe1637
Kasper4649 <imzxz123456@gmail.com>
2021-01-17 18:44:41 +0800

    [feature] delete user message

--- main.go
+++ main.go
@@ -53,21 +53,23 @@ func main() {
 			case "help":
 				response = tgbotapi.NewMessage(update.Message.Chat.ID, "禁止幫助")
 			case "decode":
-				morseCode := strings.Join(strings.Split(update.Message.Text, " ")[1:], " ")
+				morseCode := strings.TrimLeft(update.Message.Text, "/decode ")
 				if morseCode == "" {
 					response = tgbotapi.NewMessage(update.Message.Chat.ID, "勸你最好有輸入")
 					break
 				}
 				res, _ := controller.JsParser(util.StaticPath, "xmorse.decode", morseCode)
 				response = tgbotapi.NewMessage(update.Message.Chat.ID, res)
+				_, _ = bot.Send(tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID))
 			case "encode":
-				text := strings.Join(strings.Split(update.Message.Text, " ")[1:], " ")
+				text := strings.TrimLeft(update.Message.Text, "/encode ")
 				if text == "" {
 					response = tgbotapi.NewMessage(update.Message.Chat.ID, "勸你最好有輸入")
 					break
 				}
 				res, _ := controller.JsParser(util.StaticPath, "xmorse.encode", text)
 				response = tgbotapi.NewMessage(update.Message.Chat.ID, res)
+				_, _ = bot.Send(tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID))
 			default:
 				response = tgbotapi.NewMessage(update.Message.Chat.ID, "不要亂玩人家哦。")
 			}
@@ -75,7 +77,7 @@ func main() {
 			response = tgbotapi.NewMessage(update.Message.Chat.ID, "這位先生，本小姐不陪聊。")
 		}
 		
-		bot.Send(response)
+		_, _ = bot.Send(response)
 	})
 	
 	err = router.Run()

d93fa14abe550d0dc789de7f5bf1dd8edb33c4d2
Kasper4649 <imzxz123456@gmail.com>
2021-01-18 17:09:55 +0800

    [feature] add chat action

--- handler.go
+++ handler.go
@@ -3,29 +3,56 @@ package main
 import (
 	tb "gopkg.in/tucnak/telebot.v2"
 	"morse-telegram-bot/util"
+	"strings"
 )
 
 func StartHandler(m *tb.Message) {
+	if m.FromChannel() || m.FromGroup() {
+		_ = b.Notify(m.Chat, tb.Typing)
+	} else {
+		_ = b.Notify(m.Sender, tb.Typing)
+	}
 	_, _ = b.Send(m.Chat, "不準開始。")
 }
 
 func HelpHandler(m *tb.Message) {
+	if m.FromChannel() || m.FromGroup() {
+		_ = b.Notify(m.Chat, tb.Typing)
+	} else {
+		_ = b.Notify(m.Sender, tb.Typing)
+	}
 	_, _ = b.Send(m.Chat, "禁止幫助⛔。")
 }
 
 func DecodeHandler(m *tb.Message) {
+	if m.FromChannel() || m.FromGroup() {
+		_ = b.Notify(m.Chat, tb.Typing)
+	} else {
+		_ = b.Notify(m.Sender, tb.Typing)
+	}
+	
 	if m.Payload == "" {
 		_, _ = b.Send(m.Chat, "勸你最好有輸入。")
+		return
 	}
+	
 	text, _ := util.JsParser(util.StaticPath, "xmorse.decode", m.Payload)
 	_, _ = b.Reply(m, text)
 	_ = b.Delete(m)
 }
 
 func EncodeHandler(m *tb.Message) {
+	if m.FromChannel() || m.FromGroup() {
+		_ = b.Notify(m.Chat, tb.Typing)
+	} else {
+		_ = b.Notify(m.Sender, tb.Typing)
+	}
+	
 	if m.Payload == "" {
 		_, _ = b.Send(m.Chat, "勸你最好有輸入。")
+		return
 	}
+	
 	morseCode, _ := util.JsParser(util.StaticPath, "xmorse.encode", m.Payload)
 	_, _ = b.Reply(m, morseCode)
 	_ = b.Delete(m)
@@ -33,7 +60,12 @@ func EncodeHandler(m *tb.Message) {
 
 func OnTextHandler(m *tb.Message) {
 	if m.FromGroup() || m.FromChannel() {
-		return
+		if !strings.HasPrefix(m.Text, "/") {
+			return
+		}
+		_ = b.Notify(m.Chat, tb.Typing)
+	} else {
+		_ = b.Notify(m.Sender, tb.Typing)
 	}
 	_, _ = b.Send(m.Chat, "這位先生，本小姐不陪聊哦。")
 }

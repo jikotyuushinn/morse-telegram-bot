7714906c5c1a272bc3d47c4c3755c1bf84ad1b68
Kasper4649 <imzxz123456@gmail.com>
2021-08-29 20:20:33 +0800

    refactor: change heroku to vercel

--- api/api.go
+++ api/api.go
@@ -1,16 +1,16 @@
 package api
 
 import (
-	"bot"
 	"encoding/json"
 	tele "gopkg.in/tucnak/telebot.v3"
 	"io"
+	tgbot "morse-telegram-bot"
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	bot.InitConfig()
-	b, err := bot.NewBot()
+	tgbot.InitConfig()
+	b, err := tgbot.NewBot()
 	if err != nil {
 		panic(err)
 	}
